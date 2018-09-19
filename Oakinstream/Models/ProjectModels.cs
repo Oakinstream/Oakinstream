f07438e97290c228ebcb192af6a8c46ef870fb1d
Christoffer Ekström <Oakinstream@gmail.com>
2018-09-19 23:35:39 +0200

    Added ProjectModels.cs and ProjectCategoryModels.cs to the Models folder

---
+++ Oakinstream/Models/ProjectModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oakinstream.Models
+{
+    public class ProjectModels
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ProjectCategoryID { get; set; }
+        public virtual ProjectCategoryModels ProjectCategory { get; set; }
+    }
+}
