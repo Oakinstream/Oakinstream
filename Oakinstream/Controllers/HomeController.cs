4607e8e17e7efdb28bb703c0a83dd4e555f576ad
Christoffer Ekström <Oakinstream@gmail.com>
2018-09-21 13:10:35 +0200

    Added a Pin

--- Oakinstream/Controllers/HomeController.cs
+++ Oakinstream/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Oakinstream.Models;
 
 namespace Oakinstream.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(c => c.ApplicationUserId == userId).First().ID;
+            var manager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = manager.FindById(userId);
+            ViewBag.Pin = user.Pin;
             return View();
         }
     }
